fd314fe2ee5a9b10683cabd6171a28451a674860
aashish_k <aashishkathat00@gmail.com>
2024-05-14 01:14:32 +0530

    thank you email setup

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,7 +1,11 @@
+import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail';
 import { db } from '@/db';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
 import stripe, { Stripe } from 'stripe';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
 	try {
@@ -25,7 +29,7 @@ export async function POST(req: Request) {
 			const billingAddress = session.customer_details?.address;
 			const shippingAddress = session.shipping_details?.address;
 
-			await db.order.update({
+			const updatedOrder = await db.order.update({
 				where: {
 					id: orderId,
 				},
@@ -52,6 +56,25 @@ export async function POST(req: Request) {
 						},
 					},
 				},
+			});
+
+			await resend.emails.send({
+				from: 'CaseCobra <extry99@gmail.com>',
+				to: [event.data.object?.customer_details!.email],
+				subject: 'Thanks for your order',
+				react: OrderReceivedEmail({
+					orderId,
+					orderDate: updatedOrder.createdAt.toLocaleDateString(),
+					//@ts-ignore
+					shippingAddress: {
+						name: session.customer_details?.name!,
+						city: shippingAddress?.city!,
+						state: shippingAddress!.state,
+						country: shippingAddress?.country!,
+						postalCode: shippingAddress?.postal_code!,
+						street: shippingAddress?.line1!,
+					},
+				}),
 			});
 		}
 
